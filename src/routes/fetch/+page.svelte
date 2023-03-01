cd2730252a2bbc238a025a7ea87ec132ecd9612b
nick-holvast1 <nick.holvast@engineering.digital.dwp.gov.uk>
2023-03-01 21:06:03 +0000

    small style changes to fetch page

--- src/routes/fetch/+page.svelte
+++ src/routes/fetch/+page.svelte
@@ -1,22 +1,55 @@
 <script lang="ts">
-  let promise = fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
-    .then(x => x.json())
+  import type { CurrencyType } from '../../types/currency.type'
+  let disabled = false;
+  let triggered = false;
+	let promise = Promise.resolve<[]>([]);
+	
+	const fetchExchangeRates = async ():Promise<any> => {
+		const response = await self.fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+
+		if (response.ok) {
+  		return response.json();
+			
+		} else {
+			throw new Error('Error fetching exchange rate data');
+		}
+	}
+
+  const handleClick = () => {
+    promise = fetchExchangeRates();
+    disabled = true;
+    triggered = true;
+	}
+
 </script>
 
+<h1 class="p-4 m-4 max-w-md mx-auto bg-slate-800 shadow-md overflow-hidden md:max-w-2xl font-bold text-center text-lg">API call example</h1>
+
+<div>
+  <div>This page fetches Bitcoin exchange rates from Coindesk's API and displays the conversion rates for USD, GBP and EUR.</div>
+  <div>The full return value is displayed underneath.</div>
+</div>
+
+<button
+  on:click={handleClick} {disabled}
+>
+  Fetch Exchange Rates
+</button>
+
 {#await promise}
-  <!-- optionally show while promise is pending -->
-  <span>Loading currency data from coindesk...</span>
+  {#if triggered}
+    <span class="p-4 m-4 max-w-md mx-auto bg-slate-800 shadow-md overflow-hidden md:max-w-2xl font-bold text-center text-lg">Loading currency data from coindesk...</span>
+  {/if}
 {:then data}
-  <!-- promise was fulfilled -->
-  <div>
+  <div class="columns-2 lg:columns-3">
     {#each Object.entries(data.bpi) as [currency, info]}
-      <h2>{currency}</h2>
+      <h2>{info.description}</h2>
       <span>1 BTC = {info.rate} {currency}</span>
     {/each}
   </div>
-  <!-- <pre>
-    {JSON.stringify(data, null, 2)}
-  </pre> -->
+  <pre class="my-4 bg-slate-900 text-yellow-300">
+{JSON.stringify(data, null, 2)}
+  </pre>
 {:catch error}
-  <!-- optionally show something while promise was rejected -->
+  {JSON.stringify(error, null, 2)}
 {/await}
