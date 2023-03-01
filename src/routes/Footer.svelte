f2381dff7f149e4555dfe3844fd890dbfda38243
nick-holvast1 <nick.holvast@engineering.digital.dwp.gov.uk>
2023-03-01 12:15:53 +0000

    initial framework

---
+++ src/routes/Footer.svelte
@@ -0,0 +1,3 @@
+<footer class="bg-slate-900 text-neutral-100 items-center space-x-4 w-full text-center border-b border-grey p-4">
+	<span>Made by <a href="https://github.com/aNerdInTheHand" target="_blank" rel="noreferrer">aNerdInTheHand</a></span>
+</footer>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<header class="bg-slate-900 text-orange-400 flex font-bold sm:text-xl items-center space-x-4 w-full text-center border-b border-grey p-4">
+	<h1 class="bg-orange-400 text-purple-900 w-1/6 h-100 p-1 border-slate-800 border-8 sm:hidden">App</h1>
+	<h1 class="bg-orange-400 text-purple-900 w-1/6 p-1 h-100 border-slate-800 border-8 hidden sm:block">Branding Here</h1>
+	<nav class="sm:w-4/6">
+		<ul class="flex justify-center text-center items-center space-x-8 sm:space-x-12 lg:space-x-20">
+			<li class="hover:text-amber-400" aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+				<a href="/">
+					<span class="sm:hidden">
+						🏠
+					</span>
+					<span class="hidden sm:block">
+						🏠 Home
+					</span>
+				</a>
+			</li>
+			<li class="hover:text-amber-400" aria-current={$page.url.pathname.startsWith('/other') ? 'page' : undefined}>
+				<a href="/other">
+					<span class="sm:hidden">
+						🌭
+					</span>
+					<span class="hidden sm:block">
+						🌭 Other Route
+					</span>
+				</a>
+			</li>
+		</ul>
+	</nav>
+	<div class="w-1/6" />
+</header>
