4f95e276a64daca9acc1e94bb5da751793b4a635
nick-holvast1 <nick.holvast@engineering.digital.dwp.gov.uk>
2023-03-02 16:27:48 +0000

    styling on form page

--- src/routes/fetch/+page.svelte
+++ src/routes/fetch/+page.svelte
@@ -25,21 +25,27 @@
 
 <h1 class="p-4 m-4 max-w-md mx-auto bg-slate-800 shadow-md overflow-hidden md:max-w-2xl font-bold text-center text-lg">API call example</h1>
 
-<div>
+<div class="p-4 m-4 font-serif italic">
   <div>This page fetches Bitcoin exchange rates from Coindesk's API and displays the conversion rates for USD, GBP and EUR.</div>
   <div>The full return value is displayed underneath.</div>
+  <hr />
 </div>
 
-<button
-  on:click={handleClick} {disabled}
->
-  Fetch Exchange Rates
+<div class="flex">  
+  <button
+    on:click={handleClick} {disabled}
+    class="p-4 m-4 rounded max-w-md mx-auto bg-slate-800 hover:bg-slate-900 shadow-md overflow-hidden md:max-w-2xl font-bold text-center text-lg text-yellow-400 hover:text-yellow-300"
+    >
+    Fetch Exchange Rates
 </button>
+</div>
 
 {#await promise}
-  {#if triggered}
+  <div class="flex">
+    {#if triggered}
     <span class="p-4 m-4 max-w-md mx-auto bg-slate-800 shadow-md overflow-hidden md:max-w-2xl font-bold text-center text-lg">Loading currency data from coindesk...</span>
-  {/if}
+    {/if}
+  </div>
 {:then data}
   <div class="columns-2 lg:columns-3">
     {#each Object.entries(data.bpi) as [currency, info]}
